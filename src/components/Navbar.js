59c57aea29718bc037386fbfe90edaf8d9fed472
Ankur Patel <ankur4148@gmail.com>
2022-09-18 23:50:31 +0000

    Update 4 files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/Navbar.css"
+import ReorderIcon from "@material-ui/icons/Reorder";
 
 function Navbar() {
+    const [expandNavbar, setExpandNavbar] = useState(false);
+    
+    const location = useLocation();
+    
+    useEffect(()=> {
+        setExpandNavbar(false)
+    }, [location]);
+    
   return (
-    <div className="navbar">
+    <div className="navbar" id={expandNavbar ? "open" : "closed"}>
       <div className="toggleButton">
-        <button></button>
+        <button onClick={() => {
+            setExpandNavbar((prev) => !prev);
+        }}
+        >
+            <ReorderIcon />
+        </button>
       </div>
       <div className="links">
         <Link to="/">Home</Link>
